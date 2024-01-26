7a680730dae8d2d901ee9afb9ef00e77209c54f9
gaurav <raogaurav0009@gmail.com>
2024-01-26 22:53:16 +0530

    initial commit static ui work completed only api calling using redux is left

---
+++ src/pages/cart.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItem from "../components/cart-item";
+import { Link } from "react-router-dom";
+
+const cartItems = [
+  {
+    productId: "asdasdasd",
+    name: "Mackbook",
+    price: 3000,
+    quantity: 4,
+    photo: "https://m.media-amazon.com/images/I/71eXNIDUGjL._AC_UY218_.jpg",
+    stock: 10,
+  },
+];
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.18);
+const shippingCharges = 200;
+const discount = 400;
+const total = subtotal + tax + shippingCharges;
+
+const Cart = () => {
+  const [couponCode, setCoupenCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeOutId);
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
+  return (
+    <div className="cart">
+      <main>
+        {cartItems.length > 0 ? (
+          cartItems.map((i, idx) => <CartItem key={idx} cartItem={i} />)
+        ) : (
+          <h1>No item Added</h1>
+        )}
+      </main>
+      <aside>
+        <p>SubTotal: ${subtotal}</p>
+        <p>Shipping Charges: ${shippingCharges}</p>
+        <p>Tax: ${tax}</p>
+        <p>
+          Discount: <em>- ${discount}</em>
+        </p>
+        <p>
+          <b>Total: ${total}</b>
+        </p>
+        <input
+          type="text"
+          placeholder="Coupon code"
+          value={couponCode}
+          onChange={(e) => setCoupenCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ${discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+
+        {cartItems.length > 0 && <Link to="/shipping">Checkout</Link>}
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/pages/home.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
+
+const Home = () => {
+  const addToCartHandler = () => {};
+  return (
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard
+          productId="asdasdasd"
+          name="Mackbook"
+          price={4545}
+          stock={435}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/I/71eXNIDUGjL._AC_UY218_.jpg"
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
